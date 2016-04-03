c99c220651cf047c88bc1e429d715e041551120d
aryn11 <aryn11@mail.ru>
2016-04-03 18:58:57 +0600

    Add icons for menu

--- app/src/main/java/com/aryn/easycr/MainActivity.java
+++ app/src/main/java/com/aryn/easycr/MainActivity.java
@@ -132,7 +132,7 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
+
 
         } else if (id == R.id.nav_share) {
 
